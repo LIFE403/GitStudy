c6cf23fd6e133b079ec0d8c858afd5d720277dd0
LIFE403 <chaeeun481201@gmail.com>
2022-12-20 12:34:26 +0900

    fitst save

--- rhkkkkkwp/rhkkkkkwp/main.cpp
+++ rhkkkkkwp/rhkkkkkwp/main.cpp
@@ -1,9 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+#include <Windows.h>
 
 int main()
 {
-	printf("whffu");
+	int a = 0, b = 0, mathInput = 0, answer = 0;
+	char mathOutput = 0;
+
+	srand(time(NULL));
+
+	a = (rand() % 20) + 1;
+	b = (rand() % 20) + 1;
+	mathInput = rand() % 4;
+
+	switch (mathInput)
+	{
+	case 0:
+		mathOutput = '+';
+		answer = a + b;
+		break;
+	case 1:
+		mathOutput = '-';
+		answer = a - b;
+		break;
+	case 2:
+		mathOutput = '*';
+		answer = a * b;
+		break;
+	case 3:
+		mathOutput = '/';
+		answer = a / b;
+		break;
+	}
+
+	printf("[1/10] %d %c %d = %d", a, mathOutput, b, answer);
 
 	return 0;
 }
